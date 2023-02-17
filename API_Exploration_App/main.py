cb2d5d6baf2b33a9edceabdcd435e800a0d24ad4
davidland2160 <davidland2160@hotmail.com>
2023-02-16 19:21:42 -0500

    First Push

---
+++ API_Exploration_App/main.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+"""
+   :file main.py :
+"""
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def say_hi(name):
+    """
+       :param name:
+    """
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    say_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
